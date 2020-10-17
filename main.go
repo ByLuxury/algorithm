b6dc04735a5cd156d617dcbe68801e9a0e7ceb03
zhoushuai <zhoushuai@luojilab.com>
2020-10-18 00:22:24 +0800

    select for rand number

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package algorithm
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ch := make(chan int, 1)
+	for {
+		select { //在0～2中产生随机数
+		case ch <- 0:
+		case ch <- 1:
+		case ch <- 2:
+		case ch <- 3:
+		}
+		i := <-ch
+		fmt.Println("\n 产生的随机数为：", i)
+	}
+}
