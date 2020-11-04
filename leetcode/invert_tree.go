cf5495635734ad437b547710c1af39d7a99fe6df
jds <zhoushuaime@foxmail.com>
2020-11-04 17:00:22 +0800

    inver tress

---
+++ leetcode/invert_tree.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return root
+
+}
